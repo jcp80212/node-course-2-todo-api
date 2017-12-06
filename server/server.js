f8b20115fcddc10f365ef461fb680fac17b90425
jcp80212 <jcp80212@mysticruin.com>
2017-12-05 18:12:16 -0700

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -106,6 +106,19 @@ app.post('/users', (req, res) => {
   })
 });
 
+app.put('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body)
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
 
 
 
@@ -115,7 +128,17 @@ app.get('/users/me', authenticate, (req, res) => {
 
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
+app.put('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
   User.findByCredentials(body.email, body.password).then((user) => {
     return user.generateAuthToken().then((token) => {
       res.header('x-auth', token).send(user);
